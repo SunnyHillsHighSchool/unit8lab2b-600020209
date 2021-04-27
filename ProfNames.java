6c9d2127b870ec791e8d1f37630c8415ad12ecd2
Joon Kyo Kyo <>
2021-04-27 23:23:49 +0000

    Confirmed functionality of countNames

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,15 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int count = 0;
+    int length = lastName.length();
+    for(int i = 0; i < nameArray.length; i++){
+      for(int j = 0; j < nameArray[i].length; j++){
+        if(nameArray[i][j].substring(0,length).equals(lastName)){
+          count++;
+        }
+      }
+    }
+    return count;
   }
 }
